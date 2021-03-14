6448e66271b45681b253d11a65d261a357a7505a
Pavel <epiccinemabattles@gmail.com>
2021-03-15 00:31:21 +0300

    feat: add realization

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+    let reverseInt = 0;
+    let marker = n/Math.abs(n);
+    while(Math.abs(n)>0){
+        reverseInt = reverseInt*10+(n%10);
+        n=~~(n/10);
+    }
+    return reverseInt*marker;
 }
